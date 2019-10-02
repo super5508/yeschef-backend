6f9e19c8f98966f817167f2e258caa813b5af6a9
super5508 <srdev5008@gmail.com>
2019-10-02 11:25:42 +0300

    add logs

--- modules/MongoImpl/UsersMongo.js
+++ modules/MongoImpl/UsersMongo.js
@@ -1,6 +1,7 @@
 const MongoClient = require('mongodb').MongoClient;
 let config = require('../../config');
 config = config[process.env.CONFIG_ENV || "development"];
+console.log("CONFIG_ENV = " + process.env.CONFIG_ENV);
 
 const uri = config.mongo.url;
 const client = new MongoClient(uri, { useNewUrlParser: true });
