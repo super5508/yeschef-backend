07ff29f54be3c30b1e6c632bfb3842bd562b4beb
super5508 <srdev5008@gmail.com>
2019-10-07 09:11:17 +0300

    es dev/prod config

--- config.js
+++ config.js
@@ -1,11 +1,31 @@
 const production = {
     "mongo": {
         "url": "mongodb+srv://yc-be:LHxTBQ6xWfWq4PL@yeschef-users-jjwej.mongodb.net/test?retryWrites=true&w=majority"
+    },
+    "es": {
+        cloud: {
+            id: 'yeschef:dXMtZWFzdC0xLmF3cy5mb3VuZC5pbyRjMTcyZjdjYjVjYjY0YzUxYjE1MWYxNGE5Nzk0ODg3ZiQ3Yzc1OWFkYzVmOGQ0Mjk5YmYzYzRjNTQ5ZTFjYWE0Mw=='
+        },
+        auth: {
+            // apiKey: '8s1dqJAIS7KnUHdb5tfamg'
+            username: "yc-be",
+            password: "wV27Znc5LHGRdiq"
+        }
     }
 };
 const development = {
     "mongo": {
         "url": "mongodb+srv://yc-be-dev:W26Fbq9G2Xq92dH@dev-nppzv.mongodb.net/test?retryWrites=true&w=majority"
+    },
+    "es": {
+        cloud: {
+            id: 'yeschef-dev:dXMtZWFzdC0xLmF3cy5mb3VuZC5pbyRiYzAxNjdhNTU3NTM0NTY5OGRiOGExYTg3ZWI0ZWI0YyQ4NTAzZDViNDU2Zjg0MDc1OGQ1NWY5MGVkOGZmYTVjMQ=='
+        },
+        auth: {
+            // apiKey: '8s1dqJAIS7KnUHdb5tfamg'
+            username: "yc-be-dev",
+            password: "LnWJpBE4sCrYrQX"
+        }
     }
 }
 
--- modules/ESImpl/esClientWrapper.js
+++ modules/ESImpl/esClientWrapper.js
@@ -1,13 +1,6 @@
 const { Client } = require('@elastic/elasticsearch')
-const esClient = new Client({
-    cloud: {
-        id: 'yeschef:dXMtZWFzdC0xLmF3cy5mb3VuZC5pbyRjMTcyZjdjYjVjYjY0YzUxYjE1MWYxNGE5Nzk0ODg3ZiQ3Yzc1OWFkYzVmOGQ0Mjk5YmYzYzRjNTQ5ZTFjYWE0Mw=='
-    },
-    auth: {
-        // apiKey: '8s1dqJAIS7KnUHdb5tfamg'
-        username: "yc-be",
-        password: "wV27Znc5LHGRdiq"
-    }
-});
+config = config[process.env.CONFIG_ENV || "development"];
+
+const esClient = new Client();
 
 exports.esClient = esClient;
