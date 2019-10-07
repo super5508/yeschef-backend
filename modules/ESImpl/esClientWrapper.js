caaf26edb7594c8213206423619204e23b83114c
super5508 <srdev5008@gmail.com>
2019-10-07 09:13:54 +0300

    es config fix

--- modules/ESImpl/esClientWrapper.js
+++ modules/ESImpl/esClientWrapper.js
@@ -2,6 +2,6 @@ const { Client } = require('@elastic/elasticsearch');
 let config = require('../../config');
 config = config[process.env.CONFIG_ENV || "development"];
 
-const esClient = new Client();
+const esClient = new Client(config.es);
 
 exports.esClient = esClient;
